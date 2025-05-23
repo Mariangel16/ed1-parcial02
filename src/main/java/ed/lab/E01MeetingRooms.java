592588a07359b6a7ec7a808f096d20c5e838ccc4
Mariangel16 <84152916+Mariangel16@users.noreply.github.com>
2025-05-23 07:41:00 -0600

    Ejercicio 1 completo

--- src/main/java/ed/lab/E01MeetingRooms.java
+++ src/main/java/ed/lab/E01MeetingRooms.java
@@ -1,11 +1,30 @@
 package ed.lab;
 
 import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Comparator;
 
 public class E01MeetingRooms {
 
     public int minMeetingRooms(List<MeetingInterval> meetingIntervals) {
-        return -1;
-    }
+        if (meetingIntervals == null || meetingIntervals.isEmpty()) return 0;
 
+        // Ordenar por hora de inicio
+        meetingIntervals.sort(Comparator.comparingInt(MeetingInterval::startTime));
+
+        // Min Heap para almacenar tiempos de finalización
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+
+        for (MeetingInterval interval : meetingIntervals) {
+            // Si la sala más temprana está libre, se reutilizara
+            if (!minHeap.isEmpty() && minHeap.peek() <= interval.startTime()) {
+                minHeap.poll();
+            }
+            // Reservar una sala
+            minHeap.add(interval.endTime());
+        }
+
+        // El tamaño del heap indica cuántas salas se están usando en paralelo
+        return minHeap.size();
+    }
 }
